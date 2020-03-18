8e1718c5f0d96be06dd17f5737cc2451ea795599
Jason Karns <jason.karns@gmail.com>
2020-03-17 23:42:06 -0400

    Move to src and format

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const exec = require('util').promisify(require('child_process').exec);
+const exec = require('util').promisify(require('child_process').exec)
 const core = require('@actions/core')
 
-const { installNodenv } = require("./installer")
+const { installNodenv } = require('./installer')
 
-async function run() {
+async function run () {
   return Promise.resolve()
-    .then(() => installNodenv(core.getInput("nodenv-version")))
+    .then(() => installNodenv(core.getInput('nodenv-version')))
     .then(() => exec('nodenv local'))
     .then(cmd => core.setOutput('node-version', cmd.stdout.trim()))
     .catch(error => core.setFailed(error.message))
